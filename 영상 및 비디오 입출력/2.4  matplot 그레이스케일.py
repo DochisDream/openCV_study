f257b3fb5597c0816d1b99460be265bd0ee7fedb
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2019-05-17 09:59:41 +0900

    image & video

--- 영상 및 비디오 입출력/2.4  matplot 그레이스케일.py
+++ 영상 및 비디오 입출력/2.4  matplot 그레이스케일.py
@@ -11,6 +11,7 @@ imgGray = cv2.imread(imageFile, cv2.IMREAD_GRAYSCALE)
 plt.axis('off')
 
 plt.imshow(imgGray, cmap = "gray") #cmap (color map) = gray , interpolation (보간법 = 두점사이의 거리) = bicubic
+
 plt.show()
 
 """
